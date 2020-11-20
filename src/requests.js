8a13e70e160856b59f3496d14a53082a07b41c9b
Babuuh <kiharajoseph72@gmail.com>
2020-11-20 14:50:01 +0300

    initial commit

---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "1043e287b394b3ee3c8cc77116548bc5";
+ 
+const requests ={
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals:`/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActioMovies:`/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies:`/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies:`/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies:`/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries:`/discover/movie?api_key=${API_KEY}&with_genres=18`,
+}
+
+export default requests;
